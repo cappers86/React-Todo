f867b7be1f82fffa5552c2ec180f546fe63769c5
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-02 13:59:22 +0000

    Got a sumbit box rendering

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import TodoList from './components/TodoComponents/TodoList';
 import Todoform from './components/TodoComponents/TodoForm';
 import './components/TodoComponents/Todo.css';
 
-const data = [
-  {
-    id: 1,
-    task: 'Walk dog',
-    completed: false
-  },
-  {
-    id: 2,
-    task: 'Feed dog',
-    completed: false
-  },
-  {
-    id: 3,
-    task: 'Clean dishes',
-    completed: false
-  }
+// const data = [
+//   {
+//     id: 1,
+//     task: 'Walk dog',
+//     completed: false
+//   },
+//   {
+//     id: 2,
+//     task: 'Feed dog',
+//     completed: false
+//   },
+//   {
+//     id: 3,
+//     task: 'Clean dishes',
+//     completed: false
+//   }
 
-];
+// ];
 
 
 
@@ -29,22 +29,37 @@ const data = [
 
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
+    this.state ={
+      itemList: ''
+    }
   }
+
+
+addItem = itemName => {
+  this.setState({
+    itemList:[
+      ...this.state.itemList,
+      {
+        task: itemName,
+        id: Date.now(),
+        completed: false
+      }
+    ]
+    });
+};
+
+render() {
+  return (
+    <div>
+      <h1>Todo app</h1>
+      <Todoform addItem={this.addItem} />
+    </div>
+  );
+
   
-  render() {
-    return (
-      <div className="App">
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
 }
 
+}
 export default App;
-
-
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React from 'react';
 
 class TodoForm extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-        <button>Add Todo</button>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            itemList: ''
+        };
+    }
+    handleChanges = event => {
+        this.setState ({
+          itemList : event.target.value
+        })
+      }
+      handleSubmit = event => {
+        event.preventDefault();
+        if(this.state.itemList !== ''){
+          this.props.addItem(this.state.itemList);
+          this.setState({
+            itemList:''
+          });
+        }
+    }
+    render() {
+        console.log('rendering form')
+        return(
+            <form onSubmit = {this.handleSubmit}>
+                <input
+                onChange = {this.handleChanges}
+                type='text'
+                value={this.state.itemList}
+                />
+                <button>Submit</button>
+            </form>
+        );
+    }
 }
 
 export default TodoForm;
